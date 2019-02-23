6856f30e63a795a4fde5d1f4fade60c59fe40fb6
Suniel Mahesh <sunil.m@techveda.org>
2019-02-23 12:50:46 +0530

    Add basic Makefile, rules.mk, dummy app
    Added Makefile, rules.mk and dummy_app.c
    
    Signed-off-by: Suniel Mahesh <sunil.m@techveda.org>

---
+++ Makefile
@@ -0,0 +1,13 @@
+SRCS := dummy_app.c
+BINS := $(SRCS:.c=.bin)
+# $(SRCS:.c=.bin) means, take the variable value ${SRCS}, which is a string composed of words
+# separated by spaces, and for each word, replace the suffix .c with .bin => %.bin: target will
+# run
+
+all: ${BINS}
+
+show: dummy_app_disasm
+
+dummy_app.elf: start.o vector.o
+
+include rules.mk
---
+++ rules.mk
@@ -0,0 +1,42 @@
+CROSS_COMPILE := arm-linux-
+
+CC := ${CROSS_COMPILE}gcc
+AS := ${CROSS_COMPILE}as
+LD := ${CROSS_COMPILE}ld
+OBJCOPY := ${CROSS_COMPILE}objcopy
+OBJDUMP := ${CROSS_COMPILE}objdump
+
+CFLAGS := -mcpu=cortex-a8
+CFLAGS += -mfpu=neon
+ASFLAGS := -mcpu=cortex-a8
+ASFLAGS += -mfpu=neon
+LDFLAGS = -T u-boot-spl.lds -Map=$*.map
+OBJS = dummy_app.o
+
+%.bin: %.elf
+	@echo "converting .elf to .bin"
+	${OBJCOPY} --gap-fill=0xFF -O binary $< $@
+
+%.elf: %.o
+	@echo "converting .o to .elf"
+	${LD} ${LDFLAGS} $^ -o $@
+
+%.o: %.c
+	@echo "converting .c to .o"
+	${CC} ${CFLAGS} -c $< -o $@
+
+%.o: %.s
+	@echo "converting .s to .o"
+	${AS} ${ASFLAGS} -c $< -o $@
+
+#$(OBJS): timer.h bbb.h common.h debug.h interrupt.h arm.h leds.h serial.h
+# since target OBJS is depending on all header files
+# .o files will not be deleted
+
+
+%_disasm: %.elf
+	${OBJDUMP} -d $<
+
+clean:
+	@echo "cleaning"
+	rm -f *.bin *.map *.elf *.o
