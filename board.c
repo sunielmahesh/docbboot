f65cf71944a44150cf342d56e88c01d62b872664
Suniel Mahesh <sunil.m@techveda.org>
2019-02-26 18:04:30 +0530

    Enable rtc 32K OSC clock
    Enableb rtc 32K OSC clock
    
    Signed-off-by: Suniel Mahesh <sunil.m@techveda.org>

--- board.c
+++ board.c
@@ -8,6 +8,7 @@
 #include "include/doc_board.h"
 #include "include/doc_cpu.h"
 #include "include/doc_clock.h"
+#include "include/doc_davinci_rtc.h"
 
 static void watchdog_disable(void)
 {
@@ -44,21 +45,35 @@ static void uart_soft_reset(void)
         writel(regval, &uart_base->uartsyscfg);
 }
 
+static void rtc32k_unlock(struct davinci_rtc *rtc)
+{
+        /*
+         * Unlock the RTC's registers.  For more details please see the
+         * RTC_SS section of the TRM.  In order to unlock we need to
+         * write these specific values (keys) in this order.
+         */
+        writel(RTC_KICK0R_WE, &rtc->kick0r);
+        writel(RTC_KICK1R_WE, &rtc->kick1r);
+}
+
+static void rtc32k_enable(void)
+{
+        struct davinci_rtc *rtc = (struct davinci_rtc *)RTC_BASE;
+
+        rtc32k_unlock(rtc);
+
+        /* Enable the RTC 32K OSC by setting bits 3 and 6. */
+        writel((1 << 3) | (1 << 6), &rtc->osc);
+}
+
 void early_system_init(void)
 {
 	watchdog_disable();
 	set_uart_mux_conf();
 	setup_early_clocks();
 	uart_soft_reset();
-/*
- * Save the boot parameters passed from romcode.
- * We cannot delay the saving further than this,
- * to prevent overwrites.
- *
- *      save_omap_boot_params();
- */
 /* Enable RTC32K clock */
-//        rtc32k_enable();
+	rtc32k_enable();
 }
 
 void board_init_f(void)
